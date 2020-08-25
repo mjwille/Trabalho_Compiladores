30e6d2a9f85d408a194fd8b52d0f25a1fe949f67
Marcelo Jantsch Wille <mjwille@macOS.local>
2020-08-25 16:52:10 -0300

    Integração Hash e Scanner

--- hash.h
+++ hash.h
@@ -1,4 +1,5 @@
-#define HASH_SIZE 997
+#include <stdio.h>
+#include <string.h>
 
 typedef struct hash_node
 {
@@ -8,8 +9,60 @@ typedef struct hash_node
 } HASH_NODE;
 
 
-void hashInit();
-int hashAddress(char *text);
-HASH_NODE* hashFind(char *text);
-HASH_NODE* hashInsert(char *text);
-void hashPrint();
+#define HASH_SIZE 997
+
+HASH_NODE *HASH_TABLE[HASH_SIZE];
+
+
+void hashInit()
+{
+	int i;
+	for(i=0; i<HASH_SIZE; i++)
+	{
+		HASH_TABLE[i] = NULL;
+	}
+}
+
+
+int hashAddress(char *text)
+{
+	int i, addr = 1;
+	for(i=0; i<strlen(text); i++)
+	{
+		// Hash Function (entre 1 e 997)
+		addr = (addr * text[i]) % HASH_SIZE + 1;
+	}
+	return addr-1;   // Corrige para ficar entre 0 e 996
+}
+
+
+HASH_NODE* hashFind(char *text)
+{
+	return NULL;   //TODO: próximas etapas do trabalho, hashInsert nos devidos locais do scanner, better comments
+}
+
+
+HASH_NODE* hashInsert(char *text)
+{
+	int addr = hashAddress(text);
+	HASH_NODE *node = (HASH_NODE*) calloc(1, sizeof(HASH_NODE));
+
+	node->type = 1;
+	node->text = (char*) calloc(strlen(text)+1, sizeof(char));
+	strcpy(node->text, text);
+
+	node->next = HASH_TABLE[addr];
+	HASH_TABLE[addr] = node;
+
+	return node;
+}
+
+
+void hashPrint()
+{
+	int i;
+	HASH_NODE *node;
+	for(i=0; i<HASH_SIZE; i++)
+		for(node=HASH_TABLE[i]; node; node=node->next)
+			printf("HASH_TABLE[%d] has => type (%d), text (%s)\n", i, node->type, node->text);
+}
--- main.c
+++ main.c
@@ -17,41 +17,46 @@ void set_yyin(char *filename) {
 
 int main(int argc, char *argv[])
 {
-	int tok;
-
 	check_arguments(argc);
 	set_yyin(argv[1]);
+
+	int tok;
+	hashInit();
 
 	while(isRunning())
 	{
 		tok = yylex();
 		switch(tok)
 		{
-			case 256: printf("KW_CHAR");        printf(" na linha %d.\n", lineNumber); break;
-			case 257: printf("KW_INT");         printf(" na linha %d.\n", lineNumber); break;
-			case 259: printf("KW_FLOAT");       printf(" na linha %d.\n", lineNumber); break;
-			case 260: printf("KW_BOOL");        printf(" na linha %d.\n", lineNumber); break;
-			case 261: printf("KW_IF");          printf(" na linha %d.\n", lineNumber); break;
-			case 262: printf("KW_THEN");        printf(" na linha %d.\n", lineNumber); break;
-			case 263: printf("KW_ELSE");        printf(" na linha %d.\n", lineNumber); break;
-			case 264: printf("KW_WHILE");       printf(" na linha %d.\n", lineNumber); break;
-			case 265: printf("KW_LOOP");        printf(" na linha %d.\n", lineNumber); break;
-			case 266: printf("KW_READ");        printf(" na linha %d.\n", lineNumber); break;
-			case 267: printf("KW_PRINT");       printf(" na linha %d.\n", lineNumber); break;
-			case 268: printf("KW_RETURN");      printf(" na linha %d.\n", lineNumber); break;
-			case 270: printf("OP_LE");          printf(" na linha %d.\n", lineNumber); break;
-			case 271: printf("OP_GE");          printf(" na linha %d.\n", lineNumber); break;
-			case 272: printf("OP_EQ");          printf(" na linha %d.\n", lineNumber); break;
-			case 273: printf("OP_DF");          printf(" na linha %d.\n", lineNumber); break;
-			case 280: printf("TK_IDENTIFIER");  printf(" na linha %d.\n", lineNumber); break;
-			case 281: printf("LIT_INT");        printf(" na linha %d.\n", lineNumber); break;
-			case 282: printf("LIT_FLOAT");      printf(" na linha %d.\n", lineNumber); break;
-			case 283: printf("LIT_TRUE");       printf(" na linha %d.\n", lineNumber); break;
-			case 284: printf("LIT_FALSE");      printf(" na linha %d.\n", lineNumber); break;
-			case 285: printf("LIT_CHAR");       printf(" na linha %d.\n", lineNumber); break;
-			case 286: printf("LIT_STR");        printf(" na linha %d.\n", lineNumber); break;
-			case 290: printf("TK_ERROR");       printf(" na linha %d.\n", lineNumber); break;
-			default:  printf("%c", yytext[0]);  printf(" na linha %d.\n", lineNumber); break;
+			case 256: printf("KW_CHAR");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 257: printf("KW_INT");         printf(" na linha %d.\n", getLineNumber()); break;
+			case 259: printf("KW_FLOAT");       printf(" na linha %d.\n", getLineNumber()); break;
+			case 260: printf("KW_BOOL");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 261: printf("KW_IF");          printf(" na linha %d.\n", getLineNumber()); break;
+			case 262: printf("KW_THEN");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 263: printf("KW_ELSE");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 264: printf("KW_WHILE");       printf(" na linha %d.\n", getLineNumber()); break;
+			case 265: printf("KW_LOOP");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 266: printf("KW_READ");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 267: printf("KW_PRINT");       printf(" na linha %d.\n", getLineNumber()); break;
+			case 268: printf("KW_RETURN");      printf(" na linha %d.\n", getLineNumber()); break;
+			case 270: printf("OP_LE");          printf(" na linha %d.\n", getLineNumber()); break;
+			case 271: printf("OP_GE");          printf(" na linha %d.\n", getLineNumber()); break;
+			case 272: printf("OP_EQ");          printf(" na linha %d.\n", getLineNumber()); break;
+			case 273: printf("OP_DF");          printf(" na linha %d.\n", getLineNumber()); break;
+			case 280: printf("TK_IDENTIFIER");  printf(" na linha %d.\n", getLineNumber()); break;
+			case 281: printf("LIT_INT");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 282: printf("LIT_FLOAT");      printf(" na linha %d.\n", getLineNumber()); break;
+			case 283: printf("LIT_TRUE");       printf(" na linha %d.\n", getLineNumber()); break;
+			case 284: printf("LIT_FALSE");      printf(" na linha %d.\n", getLineNumber()); break;
+			case 285: printf("LIT_CHAR");       printf(" na linha %d.\n", getLineNumber()); break;
+			case 286: printf("LIT_STR");        printf(" na linha %d.\n", getLineNumber()); break;
+			case 290: printf("TK_ERROR");       printf(" na linha %d.\n", getLineNumber()); break;
+			default:  printf("%c", yytext[0]);  printf(" na linha %d.\n", getLineNumber()); break;
 		}
 	}
+
+	hashPrint();
+
+	return 0;
 }
