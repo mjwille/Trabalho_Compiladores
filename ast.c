739a6a5d5ffae4241ad7ada002153a452378f905
Marcelo Jantsch Wille <mjwille@macOS.local>
2020-09-17 04:54:49 -0300

    Comentários feitos na AST

--- ast.c
+++ ast.c
@@ -9,52 +9,77 @@
 #include <math.h>
 #include "ast.h"
 
-AST_NODE *astInsert(int type, HASH_NODE *symbol, AST_NODE *son0, AST_NODE *son1, AST_NODE *son2, AST_NODE *son3) {
-	AST_NODE *node = (AST_NODE*) calloc(1, sizeof(AST_NODE));
 
-	node->type = type;
-	node->symbol = symbol;
+// Insere nodo na AST dando seu tipo, o símbolo (que é o próprio nodo na tabela hash) e os nodos filhos
+AST_NODE *astInsert(int type, HASH_NODE *symbol, AST_NODE *son0, AST_NODE *son1, AST_NODE *son2, AST_NODE *son3) {
+	// Faz alocação em memória do nodo
+	AST_NODE *node = (AST_NODE*) calloc(1, sizeof(AST_NODE));
+	// Coloca dentro do nodo alocado os seus respectivos valores
+	node->type = type;            // o tipo definido em ast.h
+	node->symbol = symbol;        // o símbolo (ponteiro para um nodo na tabela hash)
+	// os filhos
 	node->son[0] = son0;
 	node->son[1] = son1;
 	node->son[2] = son2;
 	node->son[3] = son3;
-
+	// retorna o nodo para ser atribuído para $$ no parser, para depois ser atribuído a outro $$ em outra regra gramatical
 	return node;
 }
 
+
+// Imprime toda a AST
 void astPrint(AST_NODE *node) {
 	printf("\nABSTRACT SYNTAX TREE:\n\n");
+	// chama a função recursiva para a impressão da árvore abstrata
 	astShow(node, 0, 0, 0);
 	printf("\n");
 }
 
+
+// Função auxiliar para formatar a árvore durante a impressão da AST
 void astFormat(int spaces, int bar, int last) {
+	// Se não faz parte da primeira coluna (caso onde teria espaços para printar)
 	if(spaces > 0) {
 		int i, j, aux;
+		// primeira linha a ser impressa
 		for(i=0; i<=spaces; i++) {
+			// vai pegando as potências de 2 (pra ver se um nodo ainda tem filhos)
 			aux = 1 << (i);
+			// se 1 na posição i, é porque precisa imprimir '|' pois ainda tem filhos mais para baixo na árvore
 			if((bar & aux) == aux)
-				printf("   \u2502");
+				printf("   \u2502");     // imprime '    |'
 			else
-				printf("    ");
+				printf("    ");          // imprime '     '
 		}
-		if(last)
+
+		// caso especial em que o nodo é o último do pai na árvore
+		if(last) {
 			printf("\b\b\b\b\b\u2502\n");
-		else
-			printf("\n");
-		for(i=0; i<spaces; i++) {
-			aux = 1 << (i);
-			if((bar & aux) == aux)
-				printf("   \u2502");
-			else
-				printf("    ");
 		}
+		else {
+			printf("\n");
+		}
+
+		// segunda linha a ser impressa
+		for(i=0; i<spaces; i++) {
+			// vai pegando as potências de 2 (pra ver se um nodo ainda tem filhos)
+			aux = 1 << (i);
+			// se 1 na posição i, é porque precisa imprimir '|' pois ainda tem filhos mais para baixo na árvore
+			if((bar & aux) == aux)
+				printf("   \u2502");      // imprime '    |'
+			else
+				printf("    ");          // imprime '     '
+		}
+		// por fim, chegou no nodo e imprime '-- ', com o tipo do nodo vindo a seguir
 		printf("\b\u2514\u2500\u2500 ");
 	}
 }
 
+
+// Faz check se o nodo possui outro filho além do índice 'i' passado (um nodo pode ter no máximo 4 filhos)
 int hasAnotherSon(AST_NODE *node, int i) {
 	int j;
+	// percorre os filhos restantes para além do índice do filho atual passado como parâmetro
 	for(j=i; j<MAX_SONS; j++) {
 		if(node->son[j] != NULL)
 			return 1;
@@ -62,10 +87,14 @@ int hasAnotherSon(AST_NODE *node, int i) {
 	return 0;
 }
 
+
+// Função auxiliar para fazer a impressão (esta é a função recursiva de impressão que é chamada por astPrint)
 void astShow(AST_NODE *node, int spaces, int bar, int last) {
 
+	// imprime os espaços e as barras
 	astFormat(spaces, bar, last);
 
+	// imprime o tipo do nodo
 	switch(node->type) {
 		case AST_SYMBOL: printf("AST_SYMBOL: ");     break;
 		case AST_ADD:    printf("AST_ADD\n");        break;
@@ -75,21 +104,42 @@ void astShow(AST_NODE *node, int spaces, int bar, int last) {
 		default:         printf("AST_UNKNOWN\n");    break;
 	}
 
+	// se é um símbolo na tabela hash (tabela de símbolos), imprime ainda seu conteúdo texto
 	if(node-> symbol != NULL) {
 		printf("%s\n", node->symbol->text);
 	}
 
 	int i;
+	// percorre todos os filhos do nodo atual
 	for(i=0; i<MAX_SONS; i++) {
+		// se filho neste índice existe
 		if(node->son[i] != NULL) {
 			if(hasAnotherSon(node, i+1)) {
+				// codifica em potências de 2 que nodo atual ainda tem filho além desse (para continuar a imprimir as barras)
 				bar += pow(2, spaces);
+				// chama o print para filho
 				astShow(node->son[i], spaces+1, bar, 0);
 			}
 			else {
+				// chama o print para o último filho (1 no último argumento é o parâmetro 'last')
 				astShow(node->son[i], spaces+1, bar, 1);
 			}
+			// desfaz a codificação para este nodo (pois o próximo pode ser o último)
 			bar -= pow(2, spaces);
 		}
 	}
 }
+
+/*  Como funciona a impressão da árvore?
+ *
+ * spaces -> controlam quanto de espaço precisa printar na mesma linha (o nível na árvore)
+ * bar -> é a codificação em potências de 2.
+ *
+ * Se bar = 13 (1101) significa que as barras printadas são | x | | (x indica ausência de |)
+ * Assim, dentro do astFormat, é possível saber, quando desce, quais barras precisa printar acessando
+ * os bits.
+ *
+ * O last serve como correção de 'off-by-one-error', já que seria o último filho e o parâmetro
+ * bar não seria atualizado somente para aquele nodo específico, não imprimindo nenhuma barra
+ * acima do nodo final.
+ */
--- ast.h
+++ ast.h
@@ -8,14 +8,17 @@
 
 #include "hash.h"
 
+// Definição dos tipos que um AST_NODE pode ter
 #define AST_SYMBOL 1
 #define AST_ADD 2
 #define AST_SUB 3
 #define AST_MUL 4
 #define AST_DIV 5
 
+// Máximo de filhos que um AST_NODE pode ter
 #define MAX_SONS 4
 
+// Definição da AST
 typedef struct AST_NODE
 {
 	int type;
@@ -23,10 +26,19 @@ typedef struct AST_NODE
 	struct AST_NODE *son[MAX_SONS];
 } AST_NODE;
 
+// Insere nodo na AST dando seu tipo, o símbolo (que é o próprio nodo na tabela hash) e os nodos filhos
 AST_NODE *astInsert(int type, HASH_NODE *symbol, AST_NODE *son0, AST_NODE *son1, AST_NODE *son2, AST_NODE *son3);
+
+// Imprime toda a AST
 void astPrint(AST_NODE *node);
+
+// Função auxiliar para formatar a árvore durante a impressão da AST
 void astFormat(int spaces, int bar, int last);
+
+// Faz check se o nodo possui outro filho além do índice 'i' passado (um nodo pode ter no máximo 4 filhos)
 int hasAnotherSon(AST_NODE *node, int i);
+
+// Função auxiliar para fazer a impressão (esta é a função recursiva de impressão que é chamada por astPrint)
 void astShow(AST_NODE *node, int spaces, int bar, int last);
 
 #endif
